24c7e90f55ff278b0f35d1bd10418119257f37ed
Marcos Lima <marcoslima749@gmail.com>
2022-05-14 11:18:41 -0300

    cambiado el puerto para produccion

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ app.get('/',(req, res)=> {
 
 
 
-app.listen('4000', () => {
-    console.log('Server started on port 4000');
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server started on port ${process.env.PORT || PORT}`);
 });
 
  
